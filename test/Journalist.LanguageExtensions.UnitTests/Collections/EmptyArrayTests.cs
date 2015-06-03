b21cf375642f61ac13593625ba89da80e0357057
Anton Mednonogov <mednonogov.anton@gmail.com>
2015-06-03 18:22:22 +0300

    Test for EmtyArray.Get method was added.

---
+++ test/Journalist.LanguageExtensions.UnitTests/Collections/EmptyArrayTests.cs
@@ -0,0 +1,14 @@
+﻿using Journalist.Collections;
+using Xunit;
+
+namespace Journalist.LanguageExtensions.UnitTests.Collections
+{
+	public class EmptyArrayTests
+	{
+		[Fact]
+		public void Get_ReturnsSameInstance()
+		{
+			Assert.Same(EmptyArray.Get<int>(), EmptyArray.Get<int>());
+		}
+	}
+}
