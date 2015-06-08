7f340b10ac2a632730b1b1b0fe93c82ac74dd402
ar3cka <mednonogov.anton@gmail.com>
2015-06-09 00:04:56 +0300

    Description was added to language extensions assembly.

--- src/Journalist.LanguageExtensions/Properties/AssemblyInfo.cs
+++ src/Journalist.LanguageExtensions/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Journalist.LanguageExtensions")]
-[assembly: AssemblyDescription("Some description.")]
+[assembly: AssemblyDescription("A set of useful util types and methods for development productivity.")]
 
 [assembly: Guid("ac64be53-604d-4b64-85f3-25568211f70c")]
