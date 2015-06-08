f1d6bc308e8f004615fd34412b1005c4fff4ef40
ar3cka <mednonogov.anton@gmail.com>
2015-06-09 00:31:16 +0300

    Create nuget package for event store project.

--- src/Journalist.EventStore/Properties/AssemblyInfo.cs
+++ src/Journalist.EventStore/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Journalist.EventStore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Eventsourcing friendly eventstore for windows azure storage.")]
 
 [assembly: Guid("100bdf9b-e765-444c-9a8f-d89e5581ed33")]
