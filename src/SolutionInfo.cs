eaf754304f9d269501689d174b34de2bc0afc96c
Anton Mednonogov <mednonogov.anton@gmail.com>
2015-08-10 16:08:17 +0300

    Bump version to 0.8.3

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,12 +2,12 @@
 using System.Reflection;
 
 [assembly: AssemblyProductAttribute("Journalist")]
-[assembly: AssemblyVersionAttribute("0.8.2")]
-[assembly: AssemblyInformationalVersionAttribute("0.8.2")]
-[assembly: AssemblyFileVersionAttribute("0.8.2")]
+[assembly: AssemblyVersionAttribute("0.8.3")]
+[assembly: AssemblyInformationalVersionAttribute("0.8.3")]
+[assembly: AssemblyFileVersionAttribute("0.8.3")]
 [assembly: AssemblyCompanyAttribute("Anton Mednonogov")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.8.2";
+        internal const string Version = "0.8.3";
     }
 }
