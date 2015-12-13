fac75226a589555abf0de81b5b8c2bd0aa9bb9cb
ar3cka <mednonogov.anton@gmail.com>
2015-12-13 23:08:03 +0300

    Refactor (simplify) EventStreamConsumerStreamReaderFactory

--- src/Journalist.EventStore/Connection/EventStreamConsumerStreamReaderFactory.cs
+++ src/Journalist.EventStore/Connection/EventStreamConsumerStreamReaderFactory.cs
@@ -10,7 +10,6 @@ namespace Journalist.EventStore.Connection
     {
         private readonly EventStreamReaderId m_readerId;
         private readonly IEventJournal m_journal;
-        private readonly IEventJournalReaders m_journalReaders;
         private readonly IEventStoreConnectionState m_connectionState;
         private readonly IEventMutationPipeline m_mutationPipeline;
         private readonly EventStreamConsumerConfiguration m_configuration;
@@ -18,21 +17,18 @@ namespace Journalist.EventStore.Connection
         public EventStreamConsumerStreamReaderFactory(
             EventStreamReaderId readerId,
             IEventJournal journal,
-            IEventJournalReaders journalReaders,
             IEventStoreConnectionState connectionState,
             IEventMutationPipeline mutationPipeline,
             EventStreamConsumerConfiguration configuration)
         {
             Require.NotNull(readerId, "readerId");
             Require.NotNull(journal, "journal");
-            Require.NotNull(journalReaders, "journalReaders");
             Require.NotNull(connectionState, "connectionState");
             Require.NotNull(mutationPipeline, "mutationPipeline");
             Require.NotNull(configuration, "configuration");
 
             m_readerId = readerId;
             m_journal = journal;
-            m_journalReaders = journalReaders;
             m_connectionState = connectionState;
             m_mutationPipeline = mutationPipeline;
             m_configuration = configuration;
@@ -40,35 +36,43 @@ namespace Journalist.EventStore.Connection
 
         public async Task<IEventStreamReader> CreateAsync()
         {
-            if (await m_journalReaders.IsRegisteredAsync(m_readerId))
+            var cursor = await m_journal.OpenEventStreamCursorAsync(
+                m_configuration.StreamName,
+                m_readerId,
+                Constants.Settings.EVENT_SLICE_SIZE);
+
+            if (cursor.StreamHeader == EventStreamHeader.Unknown)
             {
-                return await CreateReaderAsync(m_configuration.StreamName, m_readerId);
+                return CreateReader(cursor);
             }
 
-            await m_journalReaders.RegisterAsync(m_readerId);
             if (m_configuration.StartReadingStreamFromEnd)
             {
-                var endOfStream = await m_journal.ReadStreamHeaderAsync(m_configuration.StreamName);
-                await m_journal.CommitStreamReaderPositionAsync(m_configuration.StreamName, m_readerId, endOfStream.Version);
-            }
-            else
-            {
-                await m_journal.CommitStreamReaderPositionAsync(m_configuration.StreamName, m_readerId, StreamVersion.Unknown);
+                if (cursor.CursorStreamVersion == StreamVersion.Start)
+                {
+                    await m_journal.CommitStreamReaderPositionAsync(
+                        m_configuration.StreamName,
+                        m_readerId,
+                        cursor.StreamVersion);
+
+                    cursor = await m_journal.OpenEventStreamCursorAsync(
+                        m_configuration.StreamName,
+                        m_readerId,
+                        Constants.Settings.EVENT_SLICE_SIZE);
+                }
             }
 
-            return await CreateReaderAsync(m_configuration.StreamName, m_readerId);
+            return CreateReader(cursor);
         }
 
-        private async Task<IEventStreamReader> CreateReaderAsync(string streamName, EventStreamReaderId readerId)
+        private IEventStreamReader CreateReader(IEventStreamCursor cursor)
         {
             return new EventStreamReader(
                 m_configuration.StreamName,
                 m_connectionState,
-                await m_journal.OpenEventStreamCursorAsync(
-                    streamName,
-                    readerId,
-                    Constants.Settings.EVENT_SLICE_SIZE),
+                cursor,
                 m_mutationPipeline);
         }
+
     }
 }
