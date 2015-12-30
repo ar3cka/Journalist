31eb243f6c3d30f57f146f5ff40f17474e2c12f8
Anton Mednonogov <mednonogov.anton@gmail.com>
2015-12-30 12:16:24 +0300

    Bump version to 0.9.2

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,12 +2,12 @@
 using System.Reflection;
 
 [assembly: AssemblyProductAttribute("Journalist")]
-[assembly: AssemblyVersionAttribute("0.9.1")]
-[assembly: AssemblyInformationalVersionAttribute("0.9.1")]
-[assembly: AssemblyFileVersionAttribute("0.9.1")]
+[assembly: AssemblyVersionAttribute("0.9.2")]
+[assembly: AssemblyInformationalVersionAttribute("0.9.2")]
+[assembly: AssemblyFileVersionAttribute("0.9.2")]
 [assembly: AssemblyCompanyAttribute("Anton Mednonogov")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.9.1";
+        internal const string Version = "0.9.2";
     }
 }
