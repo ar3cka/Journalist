cddb725b93340ff75c83de9fb53b276256fb415f
Vitaly Margelov <Margelov@avp.ru>
2016-07-25 13:38:19 +0300

    StreamConsumingNotificationListener pass stream version as a parameter to event processor, processing result extended

--- test/Journalist.EventStore.UnitTests/Infrastructure/Stubs/StreamConsumingNotificationListenerStub.cs
+++ test/Journalist.EventStore.UnitTests/Infrastructure/Stubs/StreamConsumingNotificationListenerStub.cs
@@ -1,15 +1,18 @@
 ﻿using System.Threading.Tasks;
+using Journalist.EventStore.Events;
 using Journalist.EventStore.Notifications.Listeners;
+using Journalist.EventStore.Notifications.Types;
 using Journalist.EventStore.Streams;
-using Journalist.Tasks;
 
 namespace Journalist.EventStore.UnitTests.Infrastructure.Stubs
 {
     public class StreamConsumingNotificationListenerStub : StreamConsumingNotificationListener
     {
-        protected override Task<bool> TryProcessEventFromConsumerAsync(IEventStreamConsumer consumer)
+        protected override Task<EventProcessingResult> TryProcessEventFromConsumerAsync(IEventStreamConsumer consumer, StreamVersion notificationStreamVersion)
         {
-            return ProcessingCompleted ? TaskDone.True : TaskDone.False;
+	        return ProcessingCompleted
+		        ? Task.FromResult(new EventProcessingResult(true, true))
+		        : Task.FromResult(new EventProcessingResult(false, false));
         }
 
         public bool ProcessingCompleted { get; set; }
