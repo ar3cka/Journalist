affeef9e9c998615fb72bd4db9e8e8afe27f6d35
Anton Mednonogov <mednonogov.anton@gmail.com>
2016-07-26 16:42:07 +0300

    Bump version to 0.11.0

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,12 +2,12 @@
 using System.Reflection;
 
 [assembly: AssemblyProductAttribute("Journalist")]
-[assembly: AssemblyVersionAttribute("0.10.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.10.0")]
-[assembly: AssemblyFileVersionAttribute("0.10.0")]
+[assembly: AssemblyVersionAttribute("0.11.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.11.0")]
+[assembly: AssemblyFileVersionAttribute("0.11.0")]
 [assembly: AssemblyCompanyAttribute("Anton Mednonogov")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.10.0";
+        internal const string Version = "0.11.0";
     }
 }
