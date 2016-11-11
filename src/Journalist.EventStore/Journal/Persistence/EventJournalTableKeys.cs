633408ff7c8122bd0ca298f3c545733e24fcdf3a
Anton Mednonogov <mednonogov.anton@gmail.com>
2016-11-11 15:42:00 +0300

    Delete pending notification row insertation from append operation

--- src/Journalist.EventStore/Journal/Persistence/EventJournalTableKeys.cs
+++ src/Journalist.EventStore/Journal/Persistence/EventJournalTableKeys.cs
@@ -1,16 +1,7 @@
-using Journalist.EventStore.Events;
-
 namespace Journalist.EventStore.Journal.Persistence
 {
     public static class EventJournalTableKeys
     {
         public static readonly string Header = "HEAD";
-        public static readonly string PendingNotificationPrefix = "PNDNTF|";
-
-        public static string GetPendingNotificationRowKey(StreamVersion streamVersion)
-        {
-            return PendingNotificationPrefix + streamVersion;
-
-        }
     }
 }
