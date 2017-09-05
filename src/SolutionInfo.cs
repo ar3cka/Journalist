238aaa21badb43628cde1a3e3b9edbadd00dabd6
anton mednonogov <mednonogov.anton@gmail.com>
2017-09-05 10:33:53 +0300

    Recompile library with 7.2.1 version of WindowsAzure.Storage

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,12 +2,12 @@
 using System.Reflection;
 
 [assembly: AssemblyProductAttribute("Journalist")]
-[assembly: AssemblyVersionAttribute("0.17.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.17.0")]
-[assembly: AssemblyFileVersionAttribute("0.17.0")]
+[assembly: AssemblyVersionAttribute("0.18.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.18.0")]
+[assembly: AssemblyFileVersionAttribute("0.18.0")]
 [assembly: AssemblyCompanyAttribute("Anton Mednonogov")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.17.0";
+        internal const string Version = "0.18.0";
     }
 }
